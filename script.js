434fe25eeba56dea2a814fedc8be73c6e7ac769a
Ivan P <88talem@gmail.com>
2020-05-04 18:55:39 +0300

    feat: creating edit functional

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ btn.addEventListener('click', (element) => {
     element.preventDefault();
 
     const wrap = document.createElement('ul');
+    const dateElement = document.createElement('div');
     const newElement = document.createElement('li');
     const closeBtn = document.createElement('button');
     const editBtn = document.createElement('button');
@@ -16,6 +17,7 @@ btn.addEventListener('click', (element) => {
     editBtn.innerHTML = 'Edit';
 
     wrap.classList.add('block__list-wrap');
+    dateElement.classList.add('block__list-date');
     newElement.classList.add('block__list-elem');
     closeBtn.classList.add('list__elem-btn');
     editBtn.classList.add('list__elem-edit');
@@ -23,19 +25,29 @@ btn.addEventListener('click', (element) => {
     checkBox.type = "checkbox";
     checkBox.id = "item_1";
 
-
-
     blockList.append(wrap);
+    wrap.append(dateElement);
     wrap.append(checkBox);
     wrap.append(newElement);
     wrap.append(editBtn);
     wrap.append(closeBtn);
 
+    let dateNow = new Date()
+    dateElement.innerHTML = dateNow.toLocaleDateString();
+
     newElement.innerHTML = input.value;
+
 
     closeBtn.addEventListener('click', (event) => event.target.parentElement.remove());
 
     wrap.addEventListener('change', (event) => event.target.nextElementSibling.classList.toggle('cross-line'));
+
+    // contentEditable
+    editBtn.addEventListener('click', (event) => {
+        console.log(event.target.previousElementSibling);
+        const click = event.target.previousElementSibling.contentEditable = true;
+        console.log(click)
+    });
 
 });
 
