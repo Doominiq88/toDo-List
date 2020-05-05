24a8d142ec255cfafd55ee2a53ad45062a731cd6
Ivan P <88talem@gmail.com>
2020-05-05 15:34:04 +0300

    feat: create func

--- script.js
+++ script.js
@@ -1,42 +1,69 @@
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    return element
+}
+
 const input = document.querySelector('.block__form-input');
 const btn = document.querySelector('.block__form-btn');
 const blockList = document.querySelector('.block__list');
+const form = document.querySelector('.form');
+let flag = false;
+// const elementPriority = document.getElementsByClassName('list__elem-prior');
+// console.log(elementPriority)
+
+input.addEventListener('focus', () => {
+    input.value = "";
+});
+
+// create task in form
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const wrap = createElement('ul', 'block__list-wrap');
+    const dateElement = createElement('div', 'block__list-date');
+    const newElement = createElement('li', 'block__list-elem');
+    const closeBtn = createElement('button', 'list__elem-btn');
+    const editBtn = createElement('button', 'list__elem-edit');
+    const checkBox = createElement('input', 'done-checkbox');
+    const elementPriority = createElement('div', 'list__elem-prior');
+    const wrapPriority = document.createElement('div', 'wrap__prior');
+    const inputElement1 = createElement('input', 'input-1');
+    const inputElement2 = createElement('input', 'input-2');
+    const inputElement3 = createElement('input', 'input-3');
 
 
 
-btn.addEventListener('click', (element) => {
-    element.preventDefault();
-
-    const wrap = document.createElement('ul');
-    const dateElement = document.createElement('div');
-    const newElement = document.createElement('li');
-    const closeBtn = document.createElement('button');
-    const editBtn = document.createElement('button');
-    const checkBox = document.createElement('input');
-    closeBtn.innerHTML = 'x';
-    editBtn.innerHTML = 'Edit';
-
-    wrap.classList.add('block__list-wrap');
-    dateElement.classList.add('block__list-date');
-    newElement.classList.add('block__list-elem');
-    closeBtn.classList.add('list__elem-btn');
-    editBtn.classList.add('list__elem-edit');
     closeBtn.id = "list__elem-btn";
+    inputElement1.type = "radio";
+    inputElement2.type = "radio";
+    inputElement3.type = "radio";
     checkBox.type = "checkbox";
     checkBox.id = "item_1";
 
+    // if (flag) {
     blockList.append(wrap);
+    wrap.append(elementPriority);
     wrap.append(dateElement);
     wrap.append(checkBox);
     wrap.append(newElement);
+    wrap.append(wrapPriority);
+    wrapPriority.append(inputElement1);
+    wrapPriority.append(inputElement2);
+    wrapPriority.append(inputElement3);
     wrap.append(editBtn);
     wrap.append(closeBtn);
+    // }
 
+    closeBtn.innerHTML = 'x';
+    editBtn.innerHTML = 'Edit';
+
+
+    // add date
     let dateNow = new Date()
     dateElement.innerHTML = dateNow.toLocaleDateString();
 
     newElement.innerHTML = input.value;
-
 
     closeBtn.addEventListener('click', (event) => event.target.parentElement.remove());
 
@@ -49,8 +76,16 @@ btn.addEventListener('click', (element) => {
         console.log(click)
     });
 
-});
+    //
+    // const allImputs = [...document.querySelectorAll('.input')]
+    // allImputs.forEach((element) => {
+    //     element.addEventListener('change', () => {
+    //         flag = true;
 
-input.addEventListener('focus', () => {
-    input.value = "";
+    //         elementPriority.classList.add('prior-low');
+
+    //     });
+    // });
+
+
 });
